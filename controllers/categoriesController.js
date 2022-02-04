3bae5f2e247f40e343d11132aea2cec62e059af4
BernardKwesi <bernarddormon31@gmail.com>
2022-02-04 06:06:27 +0000

    Implementing the CRUD functionality for item and categories controller

--- controllers/categoriesController.js
+++ controllers/categoriesController.js
@@ -4,31 +4,150 @@ let {body,validationResult} =require('express-validator');
 let async = require('async');
 
 exports.category_list=function(req,res,next){
-    res.json({msg:'Hello From Inventory'});
+    Category.find().exec(function (err, list_categories){
+            if(err) return next(err);
+
+            res.render('category_list',{title:'Categories List',list_categories: list_categories});
+    })
+    
+   
 }
 exports.category_create_get =function(req,res){
     res.render('category_form',{title:'Create Category'});
 }
-exports.category_create_post =function(req,res,next){
+exports.category_create_post =[
+body('name','Category Name is specified').trim().isLength({min:1}).escape(),
+body('description','Category Description is specified').trim().isLength({min:1}).escape(),
 
-};
+function(req,res,next){
+    const errors = validationResult(req);
+
+    let category = new Category({
+        name:req.body.name,
+        description:req.body.description
+    });
+
+    if(!errors.isEmpty()){
+        res.render('category_form',{title:'Create Category', errors:errors.array(),category:category});
+        return;
+    }
+    else{
+        category.save(function (err){
+            res.redirect('/inventory');
+        })
+    }
+}
+
+]
 
 exports.category_delete_get = function(req,res){
 
-    res.render('category_delete',{title:'Delete Category'});
-}
-exports.category_delete_post = function(req,res){
+    async.parallel({
+        category:function(callback) {
+            Category.findById(req.params.id).exec(callback);
+        },
+        items:function(callback){
+            Items.find({category:req.params.id}).exec(callback);
+        }
+    },
+    function(err,results){
+        if(err) return next(err);
+        if(results.category == null){
+            res.redirect('/inventory');
+        }
 
+        res.render('category_delete',{title:'Delete Category',category:results.category, items:results.items});
+    })
+   
+}
+exports.category_delete_post = function(req,res,next){
+        async.parallel({
+            category:function(callback){
+                Category.findById(req.body.category_id).exec(callback);
+            },
+            items:function(callback){
+                Items.find({'category':req.body.category_id}).exec(callback);
+            }
+        },function(err,results){
+            if(err) return next(err);
+            if(results.items.length > 0 ){
+                res.render('category_delete',{title:'Delete Category',category:results.category, items:results.items});
+            }
+            else{
+                Category.findByIdAndDelete(req.body.category_id,function(err){
+                    if(err) return next(err);
+                    
+                    res.redirect('/inventory');
+                })
+            }
+        })
 }
 
 exports.category_update_get = function(req,res){
-    res.render('category_form',{title:'Update Category'});
-}
+    async.parallel({
+        category:function(callback){
+        
+                Category.findById(req.params.id).exec(callback);
+            
+    }
+},function(err,results){
+    if(err) return next(err);
+    if(results.category == null ){
+        let err= new Error('Category Not Found');
+      err.status =404;
+      return next(err);
+    }
+ 
+    res.render('category_form',{title:'Update Category',category:results.category})
+})
+   
+    }
+    
+
 
 exports.category_update_post = function(req,res){
-   
+    body('name','Category Name is specified').trim().isLength({min:1}).escape(),
+    body('description','Category Description is specified').trim().isLength({min:1}).escape(),
+    
+    function(req,res,next){
+        const errors = validationResult(req);
+    
+        let category = new Category({
+            _id : req.params.id,
+            name:req.body.name,
+            description:req.body.description
+        });
+    
+        if(!errors.isEmpty()){
+            res.render('category_form',{title:'Create Category', errors:errors.array(),category:category});
+            return;
+        }
+        else{
+            Category.findByIdAndUpdate(req.params.id,category,{},function(err,updated){
+                if(err) return next(err)
+                res.redirect(updated.url);
+            })
+        }
+    }
+      
 }
 
 exports.category_index =function(req,res){
-    res.render('category_details',{title:'Category Details'});
+    async.parallel({
+        category:function(callback){
+            Category.findById(req.params.id).exec(callback);
+        },
+        items:function(callback){
+            Items.find({'category':req.params.id}).exec(callback);
+        }
+    },function(err,results){
+        if(err) return next(err);
+        if(results.category == null){
+            let err= new Error('Category Not Found');
+            err.status =404;
+            return next(err);
+        }
+        res.render('category_details',{title:'Category Details',category:results.category, items:results.items});
+    })
+    
 }
--- controllers/itemsController.js
+++ controllers/itemsController.js
@@ -4,23 +4,160 @@ let {body,validationResult} =require('express-validator');
 let async = require('async');
 
 exports.item_create_get =function(req,res){
-    res.render('item_form',{title:'Create Item'})
-}
-exports.item_create_post=function(req,res){
+    async.parallel({
+        category:function(callback){
+            Category.find(callback);
+        }
+    },function(err,results){
+        if(err) return next(err);
 
+        res.render('item_form',{title:'Create Item',category:results.category});
+    })
+   
 }
+exports.item_create_post=[
+    //Validate the form
+    body('name','Item Name is required').trim().isLength({min:1}).escape(),
+    body('description','Item Description is required').trim().isLength({min:1}).escape(),
+    body('category','The Category is required').trim().isLength({min:1}).escape(),
+    body('price','The Item price is required').trim().isLength({min:1}).escape(),
+    body('number_in_stock', 'THe number in stock is required').trim().isLength({min:1}).escape(),
+
+    function(req,res, next){
+        const errors = validationResult(req);
+
+        let item = new Item({
+            name: req.body.name,
+            description:req.body.description,
+            category:req.body.category,
+            price:req.body.price,
+            number_in_stock:req.body.number_in_stock
+        });
+
+        if(!errors.isEmpty()){
+            async.parallel({
+                category:function(callback){
+                    Category.find(callback);
+                }
+            },function(err,results){
+                if(err) return next(err);
+        
+                res.render('item_form',{title:'Create Item',category:results.category,item : item,error:errors.array()});
+            })
+           
+        }else{
+            item.save(function (err,savedItem){
+                if(err) return next(err);
+                res.redirect(savedItem.url);
+            })
+        }
+    }
+]
+
+
 exports.item_delete_get=function(req,res){
-    res.render('item_delete',{title:'Delete Item'});
+    async.parallel({
+        item:function(callback){
+            Item.findById(req.params.id).exec(callback);
+        }
+    },function(err,results){
+        if(err) return next(err)
+            if(results.item == null){
+                let err = new Error("Item Not found");
+                err.status = 404;
+                return next(err);
+            }
+        res.render('item_delete',{title:'Delete Item',item:results.item});
+    });
+   
 }
 exports.item_delete_post=function(req,res){
 
+    sync.parallel({
+        item:function(callback){
+            Item.findById(req.params.id).exec(callback);
+        }
+    },function(err,results){
+        if(err) return next(err)
+
+        Item.findByIdAndUpdate(req.body.item_id,function(err){
+            if(err) return next(err);
+
+            res.redirect('/inventory');
+        })
+        
+    });
+
 }
 exports.item_update_get = function(req,res){
-    res.render('item_form',{title:'Update Item'});
-}
-exports.item_update_post = function(req,res){
+    async.parallel({
+        item:function(callback){
+            Item.findById(req.params.id).populate('category').exec(callback);
+        },
+        category:function(callback){
+            Category.find(callback);
+        }
+    },function(err,results){
+        if(err) return next(err);
+        if(results.item == null){
+            let err= new Error('Item not Found');
+            err.status =404;
+            return next(err);
+        }
+        res.render('item_form',{title:'Update Item',category:results.category, item:results.item})
+    });
+    
     
 }
+
+exports.item_update_post = [
+    //Validate the form
+    body('name','Item Name is required').trim().isLength({min:1}).escape(),
+    body('description','Item Description is required').trim().isLength({min:1}).escape(),
+    body('category','The Category is required').trim().isLength({min:1}).escape(),
+    body('price','The Item price is required').trim().isLength({min:1}).escape(),
+    body('number_in_stock', 'THe number in stock is required').trim().isLength({min:1}).escape(),
+
+    function(req,res, next){
+        const errors = validationResult(req);
+
+        let item = new Item({
+            _id:req.params.id,
+            name: req.body.name,
+            description:req.body.description,
+            category:req.body.category,
+            price:req.body.price,
+            number_in_stock:req.body.number_in_stock
+        });
+
+        if(!errors.isEmpty()){
+            async.parallel({
+                category:function(callback){
+                    Category.find(callback);
+                }
+            },function(err,results){
+                if(err) return next(err);
+        
+                res.render('item_form',{title:'Update Item',category:results.category,item : item,error:errors.array()});
+            });
+           
+        }else{
+            Item.findByIdAndUpdate(req.params.id,item,{},function (err,updatedItem){
+                if(err) return next(err);
+                res.redirect(updatedItem.url);
+            });
+        }
+    }
+]
 exports.item_details_get = function(req,res){
-    res.render('item_details',{title:'Item Details'});
+    async.parallel({
+        item:function(callback){
+            Item.findById(req.params.id).populate('category').exec(callback);
+        }
+    },function(err,results){
+        if(err) return next(err);
+        res.render('item_details',{title:'Item Details',item:results.item});
+    })
+   
+    
 }
